efe4e2a152ce659e932700ea9b8ae71d4a00f9a8
janaravi10 <janaravi962@gmail.com>
2019-07-22 14:54:34 +0530

    Implemented @hapi library and replaced express

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-const express = require("express");
-const app = express(),
-  path = require("path");
+const Hapi = require("@hapi/hapi"),
+  Path = require("path"),
+  Inert = require("@hapi/inert"),
+  productRoutes = require("./product/productRoute"),
+  supplierRoutes = require("./supplier/supplierRoute");
 require("./db_connection/db");
-const cors = require("cors"),
-  corsOption = {
-    origin: "http://localhost:3000",
-    credentials: true
-  };
-const bodyParser = require("body-parser");
-const productRoute = require("./routes/productRoute");
-const supplierRoute = require("./routes/supplierRoute");
-// const projectRoute = require("./routes/projectRoute");
-// @port for server
+const cors = require("cors");
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    host: "localhost",
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, "public")
+      },
+      cors: true
+    }
+  });
+  await server.register(Inert);
 
+  server.route(productRoutes);
+  server.route(supplierRoutes);
+  server.route({
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: "."
+      }
+    }
+  });
 
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
 
-const port = process.env.PORT || 5000;
-// disabling cors
-app.use(cors(corsOption));
-// use middleware
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-// // routes for uploading product
-app.use("/product", productRoute);
-// // routes for supplier data
-app.use("/supplier", supplierRoute);
-// //routes for uploading project
-// app.use("/project", projectRoute);// ... other app.use middleware
-app.use(express.static(path.join(__dirname, "public")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,"public", "index.html"));
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
 });
-app.listen(port, () => console.log("Server started on " + port));
+
+init();
